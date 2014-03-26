dae31fdfa40a75e5d002a31485a9402d2c70779d
Trevichko <dani.v.ivanov@gmail.com>
2014-03-26 21:34:59 +0200

    skelet

---
+++ js/Objects.js
@@ -0,0 +1,73 @@
+﻿//Main Object class hae size and position and draw method (undefined)
+Object = Class.extend({
+    init: function (x,y,width,height) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+    },
+    getPosition: function () {
+        var that = this;
+        return {
+            x: that.x,
+            y: that.y,
+        };
+    },
+    getSize: function () {
+        var that = this;
+        return {
+            width: that.width,
+            height: that.height,
+        };
+    },
+    drawObj: function (canvas) {
+        var c = canvas.getContext("2d");
+    }
+});
+
+//
+MoveableObject = Object.extend({
+    init: function (x, y, width, height) {
+        this._super(x, y, width, height);
+        this.sprite = {
+            width: null,
+            height: null,
+            image: null,
+            frames: null,
+        },
+        drawSprite = function (s) {
+            //TO DO
+        }
+    },
+
+});
+
+//Interactable object to do
+InteractableObject = Object.extend({
+    init: function (x, y, width, height) {
+        this._super(x, y, width, height);
+    },
+});
+
+//
+ClickPoint = InteractableObject.extend({
+    init: function (x, y, width, height) {
+        this._super(x, y, width, height);
+    },
+    isClicked: function (mouseX, mouseY) {
+        console.log(mouseX + " " + mouseY);
+        // if x between this x and this.x + this.width AND if y between this.y and this.y+this.height
+        if ((mouseX > this.x && mouseX < (this.x + this.width)) && (mouseY > this.y && mouseY < (this.y + this.height))) {
+            console.log("hello");
+            return true;
+        }
+    },
+    drawObj: function (canvas) {
+        var c = canvas.getContext("2d");
+        c.fillRect(this.x, this.y, this.width, this.height);//testing
+    },
+
+
+});
+
+
--- js/story.js
+++ js/story.js
@@ -1,1 +1,41 @@
 ﻿
+Story = Class.extend({
+    
+    init: function () {
+        this.interactableObjects = new Array();
+        this.mainCanvas;
+    },
+    clickEvent: function (ev) {
+        var rect = this.mainCanvas.getBoundingClientRect();
+        var mouseX=ev.clientX-rect.left,
+            mouseY=ev.clientY-rect.top;
+        for (var i = 0, len = this.interactableObjects.length; i < len; i++) {
+            if (this.interactableObjects[i].isClicked(mouseX, mouseY)) {
+                //console.log(this.interactableObjects[i]);
+            }
+        }
+    },
+   
+})
+
+
+    var story = new Story();
+    var humanCastle = new ClickPoint(160, 150, 10, 10),
+        dwarfCamp=new ClickPoint(650,150,10,10)
+    function myfunction(e) {
+        story.clickEvent(e);
+    }
+
+    window.onload = function () {
+
+        story.interactableObjects.push(humanCastle);
+        story.interactableObjects.push(dwarfCamp);
+        story.mainCanvas = document.getElementById("canvas");
+        for (var i = 0, len = story.interactableObjects.length; i < len; i++) {
+            story.interactableObjects[i].drawObj(story.mainCanvas);
+        }
+        story.mainCanvas.addEventListener("click", myfunction, false);
+
+
+};
+
