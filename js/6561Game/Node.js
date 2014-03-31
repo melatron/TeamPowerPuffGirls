11fcd4786f0f1a5535266a5da09b01815850ab21
Antony <antony.dikov@live.com>
2014-03-31 15:15:05 +0300

    Update Node.js

--- js/6561Game/Node.js
+++ js/6561Game/Node.js
@@ -4,13 +4,30 @@ function Node (x, y, value) {
     this.value = value;
     this.unitedOnTurn = false;
     this.multuply = 2;
+    this.node = $("<div class = 'node'></div>");
+    this.node.html(value);
+};
+Node.prototype.removeFromCell = function() {
+    $(".row").eq(this.row).find(".cell").eq(this.col).find(".node").remove();
+};
+Node.prototype.addToCell = function () {
+    $(".row").eq(this.row).find(".cell").eq(this.col).append(this.node);
+};
+Node.prototype.transferToCell = function (row, col) {
+    $(".row").eq(row).find(".cell").eq(col).append(this.node);
 };
 Node.prototype.changeUnite = function changeUnite() {
     this.unitedOnTurn = false;
 };
 Node.prototype.moveUp = function () {
     if (this.row > 0) {
+        //this.node.animate({
+        //    "top": "57px"
+        //}, 'slow');
+        this.removeFromCell();
+        //this.css("top", "0px");
         this.row--;
+        this.addToCell();
     }
     else {
         return false;
@@ -19,7 +36,9 @@ Node.prototype.moveUp = function () {
 };
 Node.prototype.moveDown = function () {
     if (this.row < 3) {
+        this.removeFromCell();
         this.row++;
+        this.addToCell();
     }
     else {
         return false;
@@ -28,7 +47,10 @@ Node.prototype.moveDown = function () {
 };
 Node.prototype.moveLeft = function () {
     if (this.col > 0) {
+        this.removeFromCell();
         this.col--;
+        this.addToCell();
+        
     }
     else {
         return false;
@@ -37,7 +59,9 @@ Node.prototype.moveLeft = function () {
 };
 Node.prototype.moveRight = function () {
     if (this.col < 3) {
+        this.removeFromCell();
         this.col++;
+        this.addToCell();
     }
     else {
         return false;
@@ -50,8 +74,11 @@ Node.prototype.updatePosition = function () {
 };
 Node.prototype.unite = function unite(node) {
     if (node.value == this.value) {
+        this.removeFromCell();
+        node.removeFromCell();
         var newNode = new Node(node.col, node.row, node.value * node.multuply);
         newNode.unitedOnTurn = true;
+        newNode.addToCell();
         return newNode;
     }
 };
