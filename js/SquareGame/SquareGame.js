1150e92ef0dee4689b608ba9ce8c8e3f16c28d4e
6i6arka <rado.milushev@gmail.com>
2014-04-09 17:38:24 +0300

    asd

--- js/SquareGame/SquareGame.js
+++ js/SquareGame/SquareGame.js
@@ -182,12 +182,12 @@ var SquareGame = Class.extend({
     firstMap: [],
     init: function () {
         this.drawMaps = function () {
-            this.firstMap = [ [0, 0, 1, 1, 1, 1] ,
-                              [1, 1, 1, 0, 0, 1] ,
-                              [1, 0, 1, 0, 0, 1] ,
-                              [1, 1, 1, 1, 1, 1] ,
-                              [0, 1, 0, 1, 0, 0] ,
-                              [1, 1, 1, 1, 1, 1] ]
+            this.firstMap = [ [0, 0, 2, 2, 2, 2] ,
+                              [2, 2, 2, 0, 0, 2] ,
+                              [2, 0, 1, 0, 0, 2] ,
+                              [2, 2, 1, 2, 2, 2] ,
+                              [0, 2, 0, 2, 0, 0] ,
+                              [1, 2, 1, 2, 2, 1] ]
         };
 
         this.mapBoundaries = (function() {
