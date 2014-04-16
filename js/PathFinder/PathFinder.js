65c7a959c371dcbdee7994410af00757492ac6df
Antony <antony.dikov@live.com>
2014-04-17 02:13:04 +0300

    o

--- js/PathFinder/PathFinder.js
+++ js/PathFinder/PathFinder.js
@@ -63,8 +63,8 @@ PathFinder = Game.extend({
         });
         this.bottomSpikes.push({
             x: 100,
-            y: 100,
-            width: 50,
+            y: 150,
+            width: 100,
             height: 20
         });
 
