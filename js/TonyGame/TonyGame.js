ac49946d1624dfa0eaedc6c948127e2fe481228b
6i6arka <rado.milushev@gmail.com>
2014-04-20 21:30:26 +0300

    Github, you fucking piece of shit!!!

---
+++ js/TonyGame/TonyGame.js
@@ -0,0 +1,26 @@
+﻿var GameSingleton = (function () {
+
+    var instance;
+    var game = new Gamez();
+    function createInstance() {
+        // Here we will put the public methods which will be needed for the game
+        var object = {
+            name: "Ivan",
+            age: 19
+        };
+        return object;
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+
+            return instance;
+        }
+    };
+})();
+
+
+
